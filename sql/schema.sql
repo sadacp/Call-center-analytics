36ddb0961fa48194ac27291d5619947397744702
Sadaris Cabeza <sadarisc@gmail.com>
2025-05-30 11:22:08 -0500

    Update schema.sql

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,1 +1,19 @@
+CREATE DATABASE call_center; 
+ 
+USE call_center; 
+ 
+CREATE TABLE IF NOT EXISTS llamadas ( 
+    id INTEGER UNSIGNED AUTO_INCREMENT, 
+    fecha DATETIME, 
+    hora TIME, 
+    agente VARCHAR(255), 
+    duracion DOUBLE, 
+    atencion ENUM ('Si','No') NOT NULL, 
+    estado ENUM ('Abandonada','Atendida','En Espera') NOT NULL, 
+    motivo ENUM ('Otros','Reclamaciones','Solicitudes','Soporte técnico','Ventas') NOT 
+NULL, 
+    PRIMARY KEY (id) 
+    ); 
+
+DESC llamadas; 
 
